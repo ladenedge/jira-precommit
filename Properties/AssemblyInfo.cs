f9d0fcdce9eab39fc37627f8a06d760ea1ea1fcb
Jay Miller <jay.miller@digidata.com>
2015-08-20 13:53:15 -0600

    Updated version information.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,14 +8,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DigiData Corp.")]
 [assembly: AssemblyProduct("jira-precommit")]
-[assembly: AssemblyCopyright("Copyright © DigiData Corp. 2013")]
+[assembly: AssemblyCopyright("Copyright © DigiData Corp. 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("162407f1-e3b2-43bc-ae26-8fbaa96f50ba")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 [assembly: InternalsVisibleTo("DigiData.JiraPrecommit.Tests")]
